ea46a7340f983c0ab5211e50fb044dd11f14b7ab
-- C4 -- <info@c4ios.com>
2014-04-07 01:52:21 +0200

    Adding ArtMovieView
    GitHub app wouldn’t recognize the original folder copied into the
    Resonate repo, so I created a new app and will swap out the guts
    momentarily

---
+++ ArtMovieView/ArtMovieView/C4WorkSpace.h
@@ -0,0 +1,11 @@
+//
+//  C4WorkSpace.h
+//  ArtMovieView
+//
+//  Created by Slant on 2014-04-07.
+//
+
+#import "C4CanvasController.h"
+
+@interface C4WorkSpace : C4CanvasController
+@end
---
+++ ArtMovieView/ArtMovieView/C4WorkSpace.m
@@ -0,0 +1,16 @@
+//
+//  C4WorkSpace.m
+//  ArtMovieView
+//
+//  Created by Slant on 2014-04-07.
+//
+
+#import "C4Workspace.h"
+
+@implementation C4WorkSpace
+
+-(void)setup {
+    //work your magic here
+}
+
+@end
